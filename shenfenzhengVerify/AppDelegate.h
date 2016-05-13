e625c84b235e0b6bb7feb11bc4eb0e640ed5ba08
Zhangsong <cofco@cofcotekiMacBook-Pro.local>
2016-05-13 10:15:11 +0800

    初始化代码

---
+++ shenfenzhengVerify/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  shenfenzhengVerify
+//
+//  Created by Zhangsong on 15/10/20.
+//  Copyright (c) 2015年 zhangsong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ shenfenzhengVerify/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  shenfenzhengVerify
+//
+//  Created by Zhangsong on 15/10/20.
+//  Copyright (c) 2015年 zhangsong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
