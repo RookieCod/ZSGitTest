caa95e2a014a706181cc25c6273d685cb0bdd5db
RookeCod <515736146@qq.com>
2016-11-10 16:45:59 +0800

    删除Log

--- shenfenzhengVerify/ViewController.m
+++ shenfenzhengVerify/ViewController.m
@@ -15,9 +15,8 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    
-    NSLog(@"git");
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
